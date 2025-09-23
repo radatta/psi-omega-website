be093e5cfe01cacd2375b161b164bf347399c1a9
Rahul Datta <rahul3datta@gmail.com>
2025-09-22 23:16:49 -0700

    refactor: updated rush data

--- lib/rush_data.ts
+++ lib/rush_data.ts
@@ -4,7 +4,7 @@ export const currentRushData = {
     rushWeek: 'Week 2 of Fall Quarter',
     rushFlyer: '/images/rush/rush-flyer.jpg',
     rushMailingList:
-        'https://docs.google.com/forms/d/e/1FAIpQLSfn30kelbOHRGrevQ_jGyDSLr5sGsduVzPtnN5TFnToPEG1nQ/viewform?usp=sf_link',
+        'https://docs.google.com/forms/d/e/1FAIpQLSfLxr44WXiiPdT61TQfNnOZjFbItXcnBW3e0w4M2vPOJR_B6g/viewform',
     rushApplication: {
         link: 'https://docs.google.com/forms/d/e/1FAIpQLScvQsk5zOUZ-18KYJc3D4XR5ATR6GkHzEQn-Ug5E1bZdIjHCA/viewform?usp=sf_link',
         dueDate: 'October 1st @ 5:00 PM',
